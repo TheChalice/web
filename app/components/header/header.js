afbadb12013b47fdd29d1a983b55adb4b66529f2
fooles <18730471450@163.com>
2017-03-29 11:23:40 +0800

    change style

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -22,7 +22,25 @@ angular.module("console.header", [
                     $(window).resize( function(){
                         setheight();
                     });
+                    $(".zx_set_btn").on("click",function(){
+                        var width = 0;
+                        var height = $(document).height()
+                        $(this).toggleClass("zx_set_btn_rotate");
+                        if ($(this).hasClass("zx_set_btn_rotate")) {
+                            $(".create_new_nav").addClass("create_new_nav_new");
+                            width = $(window).width()-52;
 
+                        }else {
+                            $(".create_new_nav").removeClass("create_new_nav_new");
+                            width = $(window).width()-168;
+                        }
+                        $("#sidebar-container").toggleClass("sider_zx");
+                        $("#sidebar-right-fixed").toggleClass("sidebar-fixed");
+                        $('.create_new_modal > div:not(:first-child)').css({
+                            'width':width,
+                            'height':height
+                        })
+                    });
                     $(".mycheck").on("click",function(){
                         $(".zx_account_set").toggleClass("zx_account_set_hover");
                     })
