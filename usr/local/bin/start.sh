ed4f621a08e6b406c666c46ed4614fbde4d1ea16
zonesan <san@zoobook.org>
2017-09-25 11:50:16 +0800

    add volume service from env.

--- usr/local/bin/start.sh
+++ usr/local/bin/start.sh
@@ -13,6 +13,7 @@ sed -i 's/<api_proxy_addr>/'$API_PROXY_ADDR'/g' /usr/local/openresty/nginx/conf/
 sed -i 's/<api_oauth_addr>/'$API_OAUTH_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_metrics_addr>/'$API_METRICS_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_gitter_addr>/'$API_GITTER_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
+sed -i 's/<api_volume_addr>/'$API_VOLUME_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 
 
 
