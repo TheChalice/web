f935d939112c249688695d47cbe245e47377c0b3
fooles <18730471450@163.com>
2017-10-09 13:55:27 +0800

    tag name

--- app/views/service_create/service_create.js
+++ app/views/service_create/service_create.js
@@ -915,6 +915,7 @@ angular.module('console.service.create', [
                             container.truename = strname1.replace('/', "-");
                             container.image = GLOBAL.common_url +'/'+ strname1 + ':' + tag;
                             container.yesimage = GLOBAL.common_url+'/' + strname1 + ':' + tag;
+                            console.log('container.yesimage', container.yesimage);
                             //var str1 = res.imagesname.split("/");
                             //var strname1 = str1[0] + '/' + str1[1];
                             //container.truename = strname1.replace('/', "-");
