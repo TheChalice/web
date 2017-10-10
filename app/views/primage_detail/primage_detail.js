29d7542af8d68f84cf9e1195bf012e668500049e
fooles <18730471450@163.com>
2017-10-10 12:24:27 +0800

    tag name

--- app/views/primage_detail/primage_detail.js
+++ app/views/primage_detail/primage_detail.js
@@ -10,9 +10,10 @@ angular.module('console.image_detail', [
     ])
     .controller('prImageDetailCtrl', ['pubregistrytag','Confirm','ModalPullImage', '$state', 'ImageStream', '$http', 'platformone', 'platformlist', '$location', '$rootScope', '$scope', '$log', 'ImageStreamTag', '$stateParams',
         function (pubregistrytag,Confirm,ModalPullImage, $state, ImageStream, $http, platformone, platformlist, $location, $rootScope, $scope, $log, ImageStreamTag, $stateParams) {
+            var namespace=$stateParams.name.split('/')[0];
+            var name=$stateParams.name.split('/')[1];
 
-
-            pubregistrytag.get({namespace:$rootScope.namespace,name:$stateParams.name}, function (tag) {
+            pubregistrytag.get({namespace:namespace,name:name}, function (tag) {
                 console.log('tag', tag);
                 $scope.data = tag
                 $scope.tags=tag.tags
