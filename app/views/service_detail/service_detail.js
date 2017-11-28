253073a0975ecdb614efc96800445bbe83be90ee
fooles <18730471450@163.com>
2017-11-28 17:33:53 +0800

    xiugaimima

--- app/views/service_detail/service_detail.js
+++ app/views/service_detail/service_detail.js
@@ -398,7 +398,7 @@ angular.module('console.service.detail', [
                         $scope.quota.rubustCheck=true;
                         $scope.horiz=hor;
                     })
-                    console.log($scope.dc.spec.template.spec.containers[0].resources);
+                    //console.log($scope.dc.spec.template.spec.containers[0].resources);
                     if ($scope.dc.spec.template.spec.containers[0].resources&&$scope.dc.spec.template.spec.containers[0].resources.requests) {
 
 
